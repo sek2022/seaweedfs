dc677299c38a864b919afc71a3f440f0051d4205
sek2022 <sek2022ok@gmail.com>
2025-05-10 18:22:36 +0800

    Add show ec volume info

--- weed/shell/command_ec_info.go
+++ weed/shell/command_ec_info.go
@@ -4,9 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"time"
+	"sort"
+	"strings"
 
+	"github.com/seaweedfs/seaweedfs/weed/pb/master_pb"
 	"github.com/seaweedfs/seaweedfs/weed/storage/erasure_coding"
+	"github.com/seaweedfs/seaweedfs/weed/storage/types"
 )
 
 func init() {
@@ -14,6 +17,7 @@ func init() {
 }
 
 type commandEcInfo struct {
+	volumeId *uint32
 }
 
 func (c *commandEcInfo) Name() string {
@@ -21,17 +25,15 @@ func (c *commandEcInfo) Name() string {
 }
 
 func (c *commandEcInfo) Help() string {
-	return `显示当前Erasure Coding配置信息
+	return `print out ec shard distribution
 
-	ec.info
-	ec.info [-collection=""] [-fullPercent=95 -quietFor=1h]
+	ec.info [volumeId=xxx]
+		If volumeId is provided, print out the shard distribution for that specific volume.
+		Otherwise, print out the overall EC shard distribution.
 
-	此命令会显示：
-	1. 数据分片数量 (DataShardsCount)
-	2. 校验分片数量 (ParityShardsCount)
-	3. 总分片数量 (TotalShardsCount)
-	4. EC比例 (如10:4)
-	5. 待编码的卷数量 (使用-collection指定集合)
+	Example:
+		ec.info
+		ec.info volumeId=27
 `
 }
 
@@ -41,13 +43,106 @@ func (c *commandEcInfo) HasTag(CommandTag) bool {
 
 func (c *commandEcInfo) Do(args []string, commandEnv *CommandEnv, writer io.Writer) (err error) {
 	infoCommand := flag.NewFlagSet(c.Name(), flag.ContinueOnError)
-	collection := infoCommand.String("collection", "", "指定集合名称")
-	fullPercentage := infoCommand.Float64("fullPercent", 95, "卷达到最大卷大小的百分比")
-	quietPeriod := infoCommand.Duration("quietFor", time.Hour, "选择在此期间内没有写入操作的卷")
+	volumeIdFlag := infoCommand.Uint("volumeId", 0, "指定要查询的卷ID")
 	if err = infoCommand.Parse(args); err != nil {
 		return nil
 	}
 
+	var volumeId *uint32
+	if infoCommand.Parsed() && infoCommand.Lookup("volumeId").Value.String() != "0" {
+		vid := uint32(*volumeIdFlag)
+		volumeId = &vid
+	}
+
+	// collect topology information
+	topologyInfo, _, err := collectTopologyInfo(commandEnv, 0)
+	if err != nil {
+		return err
+	}
+
+	if volumeId != nil {
+		// 显示特定卷的分片分布
+		return c.showVolumeShardDistributionWithId(commandEnv, topologyInfo, writer, *volumeId)
+	}
+
+	// 显示整体 EC 分片分布
+	return c.showEcDistribution(commandEnv, topologyInfo, writer)
+}
+
+func (c *commandEcInfo) showVolumeShardDistributionWithId(commandEnv *CommandEnv, topologyInfo *master_pb.TopologyInfo, writer io.Writer, volumeId uint32) error {
+	// 按机柜和节点收集分片信息
+	type nodeShards struct {
+		nodeId   string
+		shardIds []erasure_coding.ShardId
+	}
+	rackNodes := make(map[string]map[string]*nodeShards)
+
+	for _, dc := range topologyInfo.DataCenterInfos {
+		for _, rack := range dc.RackInfos {
+			rackNodes[rack.Id] = make(map[string]*nodeShards)
+			for _, dn := range rack.DataNodeInfos {
+				if diskInfo, found := dn.DiskInfos[string(types.HardDriveType)]; found {
+					for _, shardInfo := range diskInfo.EcShardInfos {
+						if shardInfo.Id == volumeId {
+							shardBits := erasure_coding.ShardBits(shardInfo.EcIndexBits)
+							for _, shardId := range shardBits.ShardIds() {
+								if _, exists := rackNodes[rack.Id][dn.Id]; !exists {
+									rackNodes[rack.Id][dn.Id] = &nodeShards{
+										nodeId:   dn.Id,
+										shardIds: make([]erasure_coding.ShardId, 0),
+									}
+								}
+								rackNodes[rack.Id][dn.Id].shardIds = append(rackNodes[rack.Id][dn.Id].shardIds, shardId)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// 按机柜名称排序
+	var rackIds []string
+	for rackId := range rackNodes {
+		rackIds = append(rackIds, rackId)
+	}
+	sort.Strings(rackIds)
+
+	fmt.Fprintf(writer, "Volume %d shard distribution:\n", volumeId)
+	for _, rackId := range rackIds {
+		fmt.Fprintf(writer, "%s:\n", rackId)
+
+		// 获取该机柜下的所有节点
+		var nodes []*nodeShards
+		for _, node := range rackNodes[rackId] {
+			nodes = append(nodes, node)
+		}
+
+		// 按节点ID排序
+		sort.Slice(nodes, func(i, j int) bool {
+			return nodes[i].nodeId < nodes[j].nodeId
+		})
+
+		// 对每个节点的分片ID进行排序
+		for _, node := range nodes {
+			sort.Slice(node.shardIds, func(i, j int) bool {
+				return node.shardIds[i] < node.shardIds[j]
+			})
+
+			// 将分片ID转换为字符串
+			shardIdStrs := make([]string, len(node.shardIds))
+			for i, id := range node.shardIds {
+				shardIdStrs[i] = fmt.Sprintf("%d", id)
+			}
+
+			fmt.Fprintf(writer, "    %d.[%s] => %s\n", volumeId, strings.Join(shardIdStrs, ", "), node.nodeId)
+		}
+	}
+
+	return nil
+}
+
+func (c *commandEcInfo) showEcDistribution(commandEnv *CommandEnv, topologyInfo *master_pb.TopologyInfo, writer io.Writer) error {
 	fmt.Fprintf(writer, "Erasure Coding 配置信息:\n")
 	fmt.Fprintf(writer, "--------------------------------\n")
 	fmt.Fprintf(writer, "数据分片数量 (DataShardsCount): %d\n", erasure_coding.DataShardsCount)
@@ -59,32 +154,6 @@ func (c *commandEcInfo) Do(args []string, commandEnv *CommandEnv, writer io.Writ
 	fmt.Fprintf(writer, "小块大小: %d 字节 (%d MB)\n", erasure_coding.ErasureCodingSmallBlockSize, erasure_coding.ErasureCodingSmallBlockSize/1024/1024)
 	fmt.Fprintf(writer, "--------------------------------\n")
 
-	// 统计待编码的卷数量
-	if *collection != "" || len(args) > 0 {
-		// 如果命令行中有collection参数或者其他参数
-		volumeIds, err := collectVolumeIdsForEcEncode(commandEnv, *collection, *fullPercentage, *quietPeriod)
-		if err != nil {
-			return err
-		}
-
-		if len(volumeIds) == 0 {
-			fmt.Fprintf(writer, "集合 [%s] 中没有需要EC编码的卷\n", *collection)
-		} else {
-			fmt.Fprintf(writer, "集合 [%s] 中待EC编码的卷数量: %d\n", *collection, len(volumeIds))
-
-			// 显示部分卷ID（如果数量多就只显示前50个）
-			displayVolumeIds := volumeIds
-			if len(volumeIds) > 50 {
-				displayVolumeIds = volumeIds[:50]
-				fmt.Fprintf(writer, "前50个待编码的卷: %v... (共 %d 个)\n", displayVolumeIds, len(volumeIds))
-			} else {
-				fmt.Fprintf(writer, "待编码的卷: %v\n", volumeIds)
-			}
-		}
-	} else {
-		fmt.Fprintf(writer, "提示: 使用 -collection 参数查看特定集合中待EC编码的卷数量\n")
-		fmt.Fprintf(writer, "示例: ec.info -collection=myCollection -fullPercent=95 -quietFor=1h\n")
-	}
-
+	// 显示整体 EC 分片分布
 	return nil
 }
